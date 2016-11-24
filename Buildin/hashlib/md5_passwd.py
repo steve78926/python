b8d135bdcf61ef93b0c3eb481b2e3aeb64ec0cc3
songjianhao <fredmail03@126.com>
2016-11-24 18:00:29 +0800

    hashlib md5 login

---
+++ Buildin/hashlib/md5_passwd.py
@@ -0,0 +1,27 @@
+#coding:utf8
+
+import hashlib
+
+db = {}
+
+def register(username,password):
+    db[username] = get_md5(password + username + 'Salt')
+
+
+def get_md5(passwd):
+    md5 = hashlib.md5()
+    md5.update(passwd)
+    return md5.hexdigest()
+
+
+def login(username,password):
+    if db[username] == get_md5(password + username + 'Salt'):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    register('fred','123456')
+    print db
+    print login('fred','123456')
