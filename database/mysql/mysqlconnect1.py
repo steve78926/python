b5dca7bd6592a55cc6c6bb2441201980cb9ed28b
songjh <280864129@qq.com>
2016-12-08 22:46:00 +0800

    mysqlconnector2

--- database/mysql/mysqlconnect1.py
+++ database/mysql/mysqlconnect1.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import mysql.connector
 
-host = '192.168.152.132'
+#host = '192.168.152.132'
+host = '192.168.64.157'
 user = 'salt'
 password = '123456'
 database='test'
@@ -10,20 +11,23 @@ database='test'
 try:
     conn = mysql.connector.connect(host=host,user=user,password=password,database=database,use_unicode=True)    # use_unicode=True 表示使用unicode编码
     cursor = conn.cursor()
-    #createtable = 'create table user2 (id varchar(20) primary key, name varchar(20))'
-    deldata = 'delete from user2'
-    #cursor.execute(createtable)
-    cursor.execute(deldata)
+    droptab = 'DROP TABLE IF EXISTS user2'
+    cursor.execute(droptab)
+    createtable = 'create table user2 (id varchar(20) primary key, name varchar(20), age INTEGER)'
+    cursor.execute(createtable)
+
     id = 0
     while id < 100:
-        insertdata = "insert into user2 (id,name) values('%s', '%s')" % (str(id), 'user' + str(id))
+        insertdata = "insert into user2 (id,name,age) values('%s', '%s',%d)" % (str(id), 'user' + str(id),id)
         cursor.execute(insertdata)
         id += 1
     query = 'select * from user2'
     cursor.execute(query)
     data = cursor.fetchall()
-    for id, name in data:
-        print id + '    '+ name
+    for row in data:
+        for col in row:
+            print "%s, " % col
+        print "\n"
 except mysql.connector.Error as e:
     print "mysql error message: %s" % e
 finally:
