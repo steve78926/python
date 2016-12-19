103a2787c80ab4922d207677712bafa46e59f9d5
songjianhao <fredmail03@126.com>
2016-12-19 09:41:47 +0800

    star and findstr

--- cherubines/findstr.py
+++ cherubines/findstr.py
@@ -4,8 +4,7 @@
 在主串中查找子串最后出现的位置，如果find子串返回值不等于-1，递增position的位置，直到find返回-1，最终返回retp
 '''
 def find_last_str(mainstr,substr):
-    p = mainstr.find(substr,0)
-    retp = p            # retp 是准备返回的最终位置
+    retp = p = mainstr.find(substr,0)            # retp 是准备返回的最终位置
     while p != -1:
         retp = p
         p += 2          # 2是子串的长度
